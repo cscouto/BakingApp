b70351dad85ca52f64d33e22e74dc610600ad38a
Tiago Do Couto <cs.couto@outlook.com>
2018-05-16 13:10:21 -0500

    [stepdetailfragment] - implemented exoplayer

--- app/src/main/java/com/coutocode/bakingapp/step/StepDetailFragment.java
+++ app/src/main/java/com/coutocode/bakingapp/step/StepDetailFragment.java
@@ -2,6 +2,7 @@ package com.coutocode.bakingapp.step;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,12 +14,15 @@ import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -44,7 +48,45 @@ public class StepDetailFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.step_detail, container, false);
         ButterKnife.bind(this, rootView);
 
+        if (savedInstanceState != null){
+            step = savedInstanceState.getParcelable(getString(R.string.extra_step));
+        }
+
+        initializePlayer(step.videoURL);
         tvDescription.setText(step.description);
         return rootView;
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putParcelable(getString(R.string.extra_step), step);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        releasePLayer();
+    }
+
+    private void initializePlayer(String url){
+        if (exoPlayer == null){
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            exoPlayer = ExoPlayerFactory.newSimpleInstance(getActivity(), trackSelector, loadControl);
+            playerView.setPlayer(exoPlayer);
+            MediaSource mediaSource = new ExtractorMediaSource(Uri.parse(url),
+                   new DefaultDataSourceFactory(getContext(),Util.getUserAgent(getContext(),
+                           "BakingApp")),
+                    new DefaultExtractorsFactory(), null, null);
+            exoPlayer.prepare(mediaSource);
+            exoPlayer.setPlayWhenReady(false);
+        }
+    }
+
+    private void releasePLayer(){
+        exoPlayer.stop();
+        exoPlayer.release();
+        exoPlayer = null;
     }
 }
